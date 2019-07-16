7f4a17c0f763e2af33cce186efaadd9fb8629448
TomiTurunen <lich87@gmail.com>
2019-07-16 18:55:00 +0300

    separete creteforn to own

--- bloglist_front/src/App.js
+++ bloglist_front/src/App.js
@@ -1,227 +1,185 @@
-import React, { useState, useEffect } from 'react'
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Alert, Form, Button, ListGroup } from "react-bootstrap";
+import { handleNotification } from "./reducers/notificationReducer";
 import {
-	BrowserRouter as Router,
-	Route
-} from 'react-router-dom'
-import { Alert, Form, Button, ListGroup } from 'react-bootstrap'
+  initializeBlogs,
+  updateBlog,
+  updateLikes,
+  addBlog
+} from "./reducers/blogReducer";
+import { initializeUser } from "./reducers/userReducer";
+import { initializeUserList } from "./reducers/userListReducer";
+import { Blog, SingleBlog } from "./components/Blog";
+import UserList from "./components/UserList";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import Notification from "./components/Notification";
+import LoginForm from "./components/LoginForm";
+import Togglable from "./components/Togglable";
+import Navigation from "./components/Navigation";
+import CreateForm from "./components/CreateForm";
+import { useField } from "./hooks";
+import "./App.css";
 
-import { handleNotification } from './reducers/notificationReducer'
-import { initializeBlogs, updateBlog, updateLikes, addBlog } from './reducers/blogReducer'
-import { initializeUser } from './reducers/userReducer'
-import { initializeUserList } from './reducers/userListReducer'
-import { Blog, SingleBlog } from './components/Blog'
-import UserList from './components/UserList'
-import blogService from './services/blogs'
-import loginService from './services/login'
-import Notification from './components/Notification'
-import LoginForm from './components/LoginForm'
-import Togglable from './components/Togglable'
-import Navigation from './components/Navigation'
-import { useField } from './hooks'
-import './App.css'
+const App = props => {
+  const [password, setPassword] = useState("");
+  const username = useField("text");
+  const [setBlogs] = useState("");
+  const [message, setMessage] = useState(null);
 
-const App = (props) => {
-	const [password, setPassword] = useState('')
-	const [title, setTitle] = useState('')
-	const [author, setAuthor] = useState('')
-	const [url, setUrl] = useState('')
-	const username = useField('text')
-	const [setBlogs] = useState('')
-	const [message, setMessage] = useState(null)
+  useEffect(() => {
+    async function fetchAPI() {
+      await props.initializeUser();
+      await props.initializeUserList();
+      await props.initializeBlogs();
+    }
+    fetchAPI();
+  }, []);
+  const loginForm = () => {
+    return (
+      <div>
+        <Togglable buttonLabel="login">
+          <LoginForm
+            username={username.value}
+            password={password}
+            handleUsernameChange={username.onChange}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+        </Togglable>
+      </div>
+    );
+  };
 
+  if (props.user) {
+    blogService.setToken(props.user.token);
+  }
 
-	useEffect(() => {
-		async function fetchAPI() {
-			await props.initializeUser()
-			await props.initializeUserList()
-			await props.initializeBlogs()
-		}
-		fetchAPI()
-	}, [])
-	const loginForm = () => {
-		return (
-			<div>
-				<Togglable buttonLabel='login' >
-					<LoginForm
-						username={username.value}
-						password={password}
-						handleUsernameChange={username.onChange}
-						handlePasswordChange={({ target }) => setPassword(target.value)}
-						handleSubmit={handleLogin}
-					/>
-				</Togglable >
-			</div>
-		)
-	}
+  const handleLogin = async event => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username: username.value,
+        password: password
+      });
+      //Save login to cache
+      window.localStorage.setItem("loggedBlogger", JSON.stringify(user));
+      props.initializeUser();
+      username.reset();
+      setPassword("");
 
-	if (props.user) {
-		blogService.setToken(props.user.token)
-	}
+      props.handleNotification(
+        "Tervetuloa käyttämään blogilistaa, " + user.username,
+        true
+      );
+      setMessage(`welcome ${user.username}`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 10000);
+    } catch (exception) {
+      props.handleNotification(
+        "Käyttäjätunnus tai salasana on virheellinen.",
+        false
+      );
+      setTimeout(() => {
+        props.handleNotification(null, false);
+      }, 5000);
+    }
+  };
+  const userList = props.userList;
+  const usersForm = () => (
+    <div>
+      <h2>Users</h2>
+      {userList ? <UserList userList={userList} /> : ""}
+    </div>
+  );
+  const mainForm = () => (
+    <div>
+      <ListGroup>
+        {<CreateForm props={props} />}
+        {blogs.map(blog => (
+          <Blog key={blog.id} blog={blog} user={user} />
+        ))}
+      </ListGroup>
+    </div>
+  );
 
-	const handleCreate = async (event) => {
-		event.preventDefault()
-		try {
-			const blog = await blogService.create({
-				title: title,
-				author: author,
-				url: url,
-			})
-			props.addBlog(blog)
-			props.handleNotification('Blogi ' + title + ' luotu onnistuneesti!', true)
-		} catch (exception) {
-			props.handleNotification('Blogin luonti epäonnistui. Tarkista syöttämäsi tiedot.', false)
-		}
-	}
-
-	const handleLogin = async (event) => {
-		event.preventDefault()
-		try {
-			const user = await loginService.login({
-				username: username.value,
-				password: password,
-			})
-			//Save login to cache
-			window.localStorage.setItem(
-				'loggedBlogger', JSON.stringify(user)
-			)
-			props.initializeUser()
-			username.reset()
-			setPassword('')
-
-			props.handleNotification('Tervetuloa käyttämään blogilistaa, ' + user.username, true)
-			setMessage(`welcome ${user.username}`)
-			setTimeout(() => {
-				setMessage(null)
-			}, 10000)
-		} catch (exception) {
-			props.handleNotification('Käyttäjätunnus tai salasana on virheellinen.', false)
-			setTimeout(() => {
-				props.handleNotification(null, false)
-			}, 5000)
-		}
-	}
-	const userList = props.userList
-	const usersForm = () => (
-		<div>
-			<h2>Users</h2>
-			{userList ? <UserList userList={userList} /> : ''}
-		</div>
-	)
-	const mainForm = () => (
-		<div>
-			<ListGroup>
-				{createForm()}
-				{
-					blogs.map(blog =>
-						< Blog key={blog.id} blog={blog} user={user} />
-					)
-				}
-			</ListGroup>
-		</div>
-	)
-
-	const user = props.user
-	const blogs = props.blogs
-	const blogById = (blogList, id) =>
-		blogList.find(a => a.id === id)
-	const blogForm = () => (
-		<div>
-			<Router>
-				{(message &&
-					<Alert variant="success">
-						{message}
-					</Alert>
-				)}
-				<Navigation user={user} />
-				<Route exact path="/users*" render={() => usersForm()} />
-				<Route exact path="/" render={() => mainForm()} />
-				<Route exact path="/blogs/:id" render={({ match }) =>
-					<SingleBlog props={props} blog={blogById(blogs, match.params.id)} user={user} setBlogs={setBlogs} />
-				} />
-			</Router>
-		</div>
-	)
-
-	const createForm = () => (
-		<div>
-			<h2>create new</h2>
-			<Form onSubmit={handleCreate}>
-				<Form.Group>
-					<Form.Label>
-						title
-					</Form.Label>
-					<Form.Control
-						type="text"
-						value={title}
-						name="Title"
-						onChange={({ target }) => setTitle(target.value)}
-					/>
-					<Form.Label>
-						author
-					</Form.Label>
-					<Form.Control
-						type="text"
-						value={author}
-						name="Author"
-						onChange={({ target }) => setAuthor(target.value)}
-					/><Form.Label>
-						url
-					</Form.Label>
-					<Form.Control
-						type="text"
-						value={url}
-						name="Url"
-						onChange={({ target }) => setUrl(target.value)}
-					/>
-					<Button type="submit">Create</Button>
-				</Form.Group>
-			</Form>
-		</div >
-	)
-	const notification = props.notification
-	return (
-		<div className="container">
-			<h2>blogs</h2>
-			<div>
-				<Notification message={notification.message} notificationClass={notification.notificationClass} />
-			</div>
-			{user === null ? loginForm() : blogForm()}
-		</div>
-	)
-}
+  const user = props.user;
+  const blogs = props.blogs;
+  const blogById = (blogList, id) => blogList.find(a => a.id === id);
+  const blogForm = () => (
+    <div>
+      <Router>
+        {message && <Alert variant="success">{message}</Alert>}
+        <Navigation user={user} />
+        <Route exact path="/users*" render={() => usersForm()} />
+        <Route exact path="/" render={() => mainForm()} />
+        <Route
+          exact
+          path="/blogs/:id"
+          render={({ match }) => (
+            <SingleBlog
+              props={props}
+              blog={blogById(blogs, match.params.id)}
+              user={user}
+              setBlogs={setBlogs}
+            />
+          )}
+        />
+      </Router>
+    </div>
+  );
+  const notification = props.notification;
+  return (
+    <div className="container">
+      <h2>blogs</h2>
+      <div>
+        <Notification
+          message={notification.message}
+          notificationClass={notification.notificationClass}
+        />
+      </div>
+      {user === null ? loginForm() : blogForm()}
+    </div>
+  );
+};
 
 const notificationsToShow = ({ notifications }) => {
-	return notifications
-}
+  return notifications;
+};
 const blogsToShow = ({ blogs }) => {
-	return blogs
-}
+  return blogs;
+};
 const loggedUser = ({ user }) => {
-	return user
-}
+  return user;
+};
 
 const userListToShow = ({ userList }) => {
-	return userList
-}
+  return userList;
+};
 
-const mapStateToProps = (state, ) => {
-	return {
-		notification: notificationsToShow(state),
-		blogs: blogsToShow(state),
-		user: loggedUser(state),
-		userList: userListToShow(state)
-	}
-}
+const mapStateToProps = state => {
+  return {
+    notification: notificationsToShow(state),
+    blogs: blogsToShow(state),
+    user: loggedUser(state),
+    userList: userListToShow(state)
+  };
+};
 const mapDispatchToProps = {
-	handleNotification,
-	initializeBlogs,
-	updateBlog,
-	updateLikes,
-	addBlog,
-	initializeUser,
-	initializeUserList,
-}
-const ConnectedForm = connect(mapStateToProps, mapDispatchToProps)(App)
+  handleNotification,
+  initializeBlogs,
+  updateBlog,
+  updateLikes,
+  addBlog,
+  initializeUser,
+  initializeUserList
+};
+const ConnectedForm = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
-export default ConnectedForm
+export default ConnectedForm;
--- bloglist_front/src/reducers/blogReducer.js
+++ bloglist_front/src/reducers/blogReducer.js
@@ -1,74 +1,77 @@
 /* eslint-disable no-case-declarations */
-import blogService from '../services/blogs'
+import blogService from "../services/blogs";
 
-let initialState = []
+let initialState = [];
 export const initializeBlogs = () => {
-	return async dispatch => {
-		const blogs = await blogService.getAll()
-		initialState = blogs
-		dispatch({
-			type: 'INIT_BLOGS',
-			data: blogs,
-		})
-	}
-}
+  return async dispatch => {
+    const blogs = await blogService.getAll();
+    initialState = blogs;
+    dispatch({
+      type: "INIT_BLOGS",
+      data: blogs
+    });
+  };
+};
 
 export const updateBlog = (blog, comment) => {
-	return async dispatch => {
-		dispatch({
-			type: 'UPDATE_BLOG',
-			blog: blog,
-			comment: comment,
-		})
-	}
-}
+  return async dispatch => {
+    dispatch({
+      type: "UPDATE_BLOG",
+      blog: blog,
+      comment: comment
+    });
+  };
+};
 
-export const addBlog = (blog) => {
-	return async dispatch => {
-		dispatch({
-			type: 'CREATE',
-			blog: blog
-		})
-	}
-}
+export const addBlog = blog => {
+  console.log("Add blog", blog);
+  return async dispatch => {
+    dispatch({
+      type: "CREATE",
+      blog: blog
+    });
+  };
+};
 
-export const updateLikes = (blog) => {
-	return async dispatch => {
-		dispatch({
-			type: 'UPDATE_BLOG',
-			blog: blog,
-			likes: true
-		})
-	}
-}
+export const updateLikes = blog => {
+  return async dispatch => {
+    dispatch({
+      type: "UPDATE_BLOG",
+      blog: blog,
+      likes: true
+    });
+  };
+};
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-	case 'INIT_BLOGS':
-		return action.data
-	case 'CREATE':
-		console.log("create")
-		return state.concat(action.blog)
-	case 'UPDATE_BLOG':
-		const id = action.blog.id
-		const blogToChange = state.find(a => a.id === id)
-		let changedBlog
-		
-		changedBlog = action.likes ? {
-			...blogToChange,
-			likes: blogToChange.likes + 1
-		} :
-		{
-			...blogToChange,
-			comments: blogToChange.comments.concat(action.comment)
-		}
-		const blogs = state.map(blog =>
-			blog.id !== id ? blog : changedBlog)
-		blogs.sort((a, b) => b.votes - a.votes)
+  console.log("reducer");
+  switch (action.type) {
+    case "INIT_BLOGS":
+      return action.data;
+    case "CREATE":
+      console.log("create");
+      return state.concat(action.blog);
+    case "UPDATE_BLOG":
+      const id = action.blog.id;
+      const blogToChange = state.find(a => a.id === id);
+      let changedBlog;
 
-		return blogs
-	default: return state
-	}
-}
+      changedBlog = action.likes
+        ? {
+            ...blogToChange,
+            likes: blogToChange.likes + 1
+          }
+        : {
+            ...blogToChange,
+            comments: blogToChange.comments.concat(action.comment)
+          };
+      const blogs = state.map(blog => (blog.id !== id ? blog : changedBlog));
+      blogs.sort((a, b) => b.votes - a.votes);
 
-export default reducer
+      return blogs;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
